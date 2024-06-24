fc84b01955453c1935f8915b1908404bb426f882
unknown <ichsan.haekal23@gmail.com>
2024-06-25 00:05:45 +0700

    add: materi void cpp dan fix: while pbo

---
+++ Semester/1/Algoritma-dan-Pemograman/void.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+void banner(string name, string cls) {
+    cout << "Nama: " << name << endl;
+    cout << "kelas: " << cls << endl;
+}
+int main() {
+banner("Muhammad Ichsan Haekal", "01SISP001");
+return 0;
+}
--- Semester/1/PBO-Java-I/belajar/while.java
+++ Semester/1/PBO-Java-I/belajar/while.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 
 public class while {
     public static void main(String[] args) {
-        /*
-        * Operator Logika
-        * && mengembalikan nilai benar jika kedua pernyataan bernilai benar ( x < 6 && x < 10)
-        * || mengembalikan nilai benar jika salah satu pernyataan bernilai benar (x > 10 || x < 15)
-        * ! mengembalikan hasil, mengembalikan salah jika hasilnya nilai benar (x = true)
-        * */
         Scanner input = new Scanner(System.in);
         boolean stat = true;
         //while menggunakan input lalu cek menggunakan if percabangan
